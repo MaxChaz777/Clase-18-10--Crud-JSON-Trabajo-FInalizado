954c7d5b603c6a5baa2331a21432cc956d3eef44
Maximiliano J. Chazarreta Bernald <78647528+Maxjchazd@users.noreply.github.com>
2025-04-09 16:52:16 -0300

    Update nav.js

--- WebEcuelaJson/js/nav.js
+++ WebEcuelaJson/js/nav.js
@@ -2,10 +2,12 @@ const $$Nav=function(){
 
     this.init = () => {
         $ds.nav("body");
-        $dn.makeButton("Inicio",$ds.home);
-        $dn.makeButton("Carrera",$f.addUser);
-        $dn.makeButtonLogin("Ingresar")
-        $dn.makeDropDown("Funciones",["Agregar usuario","Buscar Usuario"],[$f.addUser,$f.findUser]);
+        $dn.makeButton("Inicio", $ds.home);
+        $dn.makeButtonLogin("Ingresar");
+        $dn.makeDropDown("Carreras", ["Agregar carrera", "Buscar Carrera"], [$f.addCarrera, $f.findCarrera]);
+        $dn.makeDropDown("Usuarios",["Agregar usuario","Buscar Usuario"],[$f.addUser,$f.findUser]);
+
+        $dn.makeDropDown("Listas",["Listas de usuarios","Listas de carreras"],[$f.ListUsers, $f.ListCarreras]);
 
     }
 
